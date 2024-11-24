65555965fd7a0522a21e842482deda4094ba5c03
HasnanAlii <ahasnan@gmail.com>
2024-11-24 22:36:59 +0700

    delete

--- tubes_janjur/android/app/src/main/kotlin/com/example/tubes_janjur/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tubes_janjur
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- tubes_janjur/lib/utils/cost.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kButtonColor = Color.fromARGB(255, 240, 94, 10);
-const kBackgroundColor = Color.fromRGBO(219, 228, 228, 1);
-const kTextColor = Color.fromARGB(255, 37, 37, 37);
